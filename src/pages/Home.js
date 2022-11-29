b6dd04356195faa50dc98cb20d54add3d2e12d15
mahesh jangid <mesh.jangid@gmail.com>
2022-11-29 16:12:24 +0530

    Init

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from "react";
-import Cardscg from "../components/Cardscg";
-import CgDiv from "../components/CgDiv";
 import ProductsC from "../components/ProductsC";
 import { Helmet } from "react-helmet";
-import { Link } from "react-router-dom";
+
 import "./Home.css";
 import { Divider } from "@chakra-ui/react";
 
